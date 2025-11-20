ed6dcdbaf8b40f9562061b60fd116b6f69bce984
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-11-20 13:42:33 -0300

    Criação das tabelas orcamento e itens_orcamento

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE orcamento (
+    id IDENTITY PRIMARY KEY,
+
+    nome_cliente VARCHAR(40) NOT NULL,
+    fone_cliente VARCHAR(20),
+    email_cliente VARCHAR(80),
+
+    descricao VARCHAR(255),
+    valor_total NUMERIC(10,2),
+    status VARCHAR(20) NOT NULL DEFAULT 'ABERTO',
+
+    dt_entrada TIMESTAMP NOT NULL,
+    dt_saida TIMESTAMP
+);
+
+CREATE TABLE itens_orcamento (
+    id IDENTITY PRIMARY KEY,
+    orcamento_id BIGINT NOT NULL,
+
+    nome VARCHAR(60) NOT NULL,
+    quantidade INT NOT NULL DEFAULT 1,
+    valor_unit NUMERIC(10,2) NOT NULL,
+    valor_total NUMERIC(10,2) NOT NULL,
+
+    CONSTRAINT fk_orcamento
+        FOREIGN KEY (orcamento_id)
+        REFERENCES orcamento(id)
+        ON DELETE CASCADE
+);
