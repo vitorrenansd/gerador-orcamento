c161290b905cc8cdda38b951846625c20fb447eb
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-11-20 12:38:27 -0300

    Criação do projeto via Spring Initialzr

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.7"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.vitorrenansd"
+version = "0.0.1-SNAPSHOT"
+description = "Sistema gerador de orçamento genérico, backend Java"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	runtimeOnly("com.h2database:h2")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/com/vitorrenansd/orcamento/OrcamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorrenansd.orcamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
