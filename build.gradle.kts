630ad37f68fbdf8e6efea400188606282847c63a
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-11-20 23:47:43 -0300

    Correção do pacote no build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "com.vitorrenansd"
+group = "com.vitordias"
 version = "0.0.1-SNAPSHOT"
 description = "Sistema gerador de orçamento genérico, backend Java"
 
