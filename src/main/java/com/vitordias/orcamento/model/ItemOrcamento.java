42ff54d6e9bb519d3088c1573f6829bcde080705
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-11-21 08:22:22 -0300

    Criados os metodos get/set da classe

--- src/main/java/com/vitordias/orcamento/model/ItemOrcamento.java
+++ src/main/java/com/vitordias/orcamento/model/ItemOrcamento.java
@@ -11,5 +11,45 @@ public class ItemOrcamento {
     private BigDecimal valorUnit;
     private BigDecimal valorTotal;
 
-    // Gerar os get/set
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getOrcamentoId() {
+        return orcamentoId;
+    }
+    public void setOrcamentoId(Integer orcamentoId) {
+        this.orcamentoId = orcamentoId;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getValorUnit() {
+        return valorUnit;
+    }
+    public void setValorUnit(BigDecimal valorUnit) {
+        this.valorUnit = valorUnit;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
 }
